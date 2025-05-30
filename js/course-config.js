08589b5b1796dddd402b523288fc522041be31e7
Yant. <1811879068@qq.com>
2025-05-30 17:23:18 +0800

    完善课程具体内容

---
+++ js/course-config.js
@@ -0,0 +1,9 @@
+// js/course-config.js
+const courseMap = {
+  "course1": "../courses/gaodengshuxue.html",
+  "course2": "../courses/xianxingdaishu.html",
+  "course3": "../courses/gailvlun.html",
+  "course4": "../courses/lisuan.html",
+  "course5": "../courses/fubian.html",
+  "course6": "../courses/weifenfangcheng.html"
+};
